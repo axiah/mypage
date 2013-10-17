da3d9cff9294898e0b56047000ee51f5f827922c
TerryAnne Bateman <terryannebateman@gmail.com>
2013-10-17 13:57:09 -0600

    fixing gitignore issues

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mypage::Application.config.secret_key_base = '33a999c8734a827a8920f9f0acf959f814c564fc6924fb6de4ba15c9b2e785da92f79781167fa1d2d486f8e6acd0c187f805a84dbc29e2a77426c5653b8cbe6a'
